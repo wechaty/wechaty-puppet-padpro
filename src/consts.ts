42fc9484b043e3daa0d9345d5caed55cf9437678
Yuan Gao <wind.memory.cn@gmail.com>
2018-10-28 19:27:35 +0800

    add consts

---
+++ src/consts.ts
@@ -0,0 +1,10 @@
+export const WX_LONG_HOST = 'long.weixin.qq.com'
+export const WX_SHORT_HOST = 'short.weixin.qq.com'
+
+export const SPECIALUSERS = ['newsapp', 'fmessage', 'filehelper', 'weibo', 'qqmail', 'fmessage', 'tmessage',
+  'qmessage', 'qqsync', 'floatbottle', 'lbsapp', 'shakeapp', 'medianote', 'qqfriend',
+  'readerapp', 'blogapp', 'facebookapp', 'masssendapp', 'meishiapp', 'feedsapp', 'voip',
+  'blogappweixin', 'weixin', 'brandsessionholder', 'weixinreminder', 'wxid_novlwrv3lqwv11',
+  'gh_22b87fa7cb3c', 'officialaccounts', 'notification_messages', 'wxid_novlwrv3lqwv11',
+  'gh_22b87fa7cb3c', 'wxitil', 'userexperience_alarm', 'notification_messages'
+]
