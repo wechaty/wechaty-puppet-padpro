bee7472d99d66d396e942be2748b8bfaa643ec80
Yuan Gao <wind.memory.cn@gmail.com>
2019-05-09 12:08:40 +0800

    add token related error message

--- src/consts.ts
+++ src/consts.ts
@@ -14,3 +14,38 @@ export const CLIENT_OS_TYPE = 'iPad iPhone OS9.3.3'
 export const CDN_USER_KEY = 'wxusrname2016cdn'
 
 export const CDN_USER_MD5_KEY = 'cdnwx2013usrname'
+
+export const INVALID_TOKEN_MESSAGE = `
+
+
+===========================================================================================
+
+      The token is invalid, please use an valid token to access padchat
+      If you have question about this, please check out our wiki:
+      https://github.com/botorange/wechaty-puppet-padpro/wiki/Buy-Padpro-Token
+
+      你使用的Token是无效的，请你联系我们获取有效Token，详情请参考这个页面：
+      https://github.com/botorange/wechaty-puppet-padpro/wiki/%E8%B4%AD%E4%B9%B0token
+
+============================================================================================
+
+
+`
+
+export const EXPIRED_TOKEN_MESSAGE = `
+
+
+===========================================================================================
+
+      The token you are using is expired, please contact us and renew this token
+      Here is our wiki, you can find the contact info here:
+      https://github.com/botorange/wechaty-puppet-padpro/wiki/Buy-Padpro-Token
+
+      你使用的Token已经过期了，如果你想继续使用wechaty-puppet-padpro，请续费你的Token
+      详情请参考以下这个页面
+      https://github.com/botorange/wechaty-puppet-padpro/wiki/%E8%B4%AD%E4%B9%B0token
+
+============================================================================================
+
+
+`
