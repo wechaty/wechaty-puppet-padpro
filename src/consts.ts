9ade3235edc3736808c4c948b91d2e991afafe74
SuperChang <suchang.soul@gmail.com>
2019-08-06 15:45:04 +0800

    modify: change the EXPIRED_TOKEN_MESSAGE (#184)

--- src/consts.ts
+++ src/consts.ts
@@ -37,13 +37,13 @@ export const EXPIRED_TOKEN_MESSAGE = `
 
 ===========================================================================================
 
-      The token you are using is expired, please contact us and renew this token
-      Here is our wiki, you can find the contact info here:
-      https://github.com/botorange/wechaty-puppet-padpro/wiki/Buy-Padpro-Token
+      The token you are using is expired, please renew this token
+      You can renew your token by yourself, here is our pay/renew token link
+      https://token.juzi.bot/
 
       你使用的Token已经过期了，如果你想继续使用wechaty-puppet-padpro，请续费你的Token
-      详情请参考以下这个页面
-      https://github.com/botorange/wechaty-puppet-padpro/wiki/%E8%B4%AD%E4%B9%B0token
+      点击下方链接，进行自助续费
+      https://token.juzi.bot/
 
 ============================================================================================
 
